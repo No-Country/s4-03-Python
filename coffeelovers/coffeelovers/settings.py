1d41e0f9f87aa984e61e32c4e74affc905db78ff
Rene Silva <rene.scre@gmail.com>
2022-10-12 21:31:09 -0500

    Landing page function

--- coffeelovers/coffeelovers/settings.py
+++ coffeelovers/coffeelovers/settings.py
@@ -60,6 +60,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'homepage',
     'useraccount',
+    'coffee_house',
+    'app_config',
+    'favorite',
 ]
 
 MIDDLEWARE = [
@@ -77,7 +80,7 @@ ROOT_URLCONF = 'coffeelovers.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -145,13 +148,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 
 )
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
