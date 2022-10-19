d305bcebbb955b3174a3c30ddb10f8251c0c9226
Rene Silva <rene.scre@gmail.com>
2022-10-18 23:21:07 -0500

    favotires functionality added

--- coffeelovers/favorite/models.py
+++ coffeelovers/favorite/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from coffee_house.models import CoffeeHouse
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Favorite(models.Model):
     SI = True
@@ -10,12 +11,12 @@ class Favorite(models.Model):
       (SI,'Si'),
       (NO,'No'),
     ]
-    house = models.ForeignKey(CoffeeHouse, on_delete=models.CASCADE, to_field='id')
-    name = models.ForeignKey(User, on_delete=models.CASCADE)
+    house = models.ForeignKey(CoffeeHouse, on_delete=models.CASCADE, null=True, to_field='id')
+    name = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     is_active = models.BooleanField(choices=YES_OR_NOT_CHOICES, default=SI)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_at = models.DateTimeField(auto_now_add=False, auto_now=True)
     
     def __str__(self):
-        return str(self.house) 
+        return "Favorites: "+ str(self.house) 
     
