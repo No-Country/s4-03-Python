5b2e11ee580c1179bc372557071cb9f4a23cb330
Rene Silva <rene.scre@gmail.com>
2022-10-20 13:02:08 -0500

    delete favorites

--- coffeelovers/favorite/urls.py
+++ coffeelovers/favorite/urls.py
@@ -5,4 +5,5 @@ app_name = 'favorite'
 urlpatterns=[
         path('list/<int:id>', views.list,name='list'),
         path('add_favorites/<int:id>',views.add_favorites,name='add_favorites'),
+        path('delete_favorite/<int:id>',views.delete_favorite,name='delete_favorite'),
     ]
--- coffeelovers/favorite/views.py
+++ coffeelovers/favorite/views.py
@@ -8,14 +8,31 @@ from django.db.models import Q
 from django.urls import reverse_lazy, reverse
 # Create your views here.
 
+def delete_favorite(request, id):
+    if not request.user.is_authenticated:
+        messages.error(request,'Accion prohibida: No está autenticado')
+        return redirect('home')
+      
+    house = CoffeeHouse.objects.get(id=id)
+    name= User.objects.get(username=request.user.username)
+    data_set = Favorite.objects.get(name=name, house=house, is_active=True)
+    if data_set.delete():
+        messages.success(request,'Felicidades, eliminaste la cafetería de favoritas')
+    
+    result_set = Favorite.objects.filter(name_id=name, is_active=True)
+    context ={'coffees': result_set, 'detail_id': 9999 }
+    return render(request, 'favorite/favorites_list.html',context)
+  
+  
+    
 def list(request, id):
     if not request.user.is_authenticated:
         messages.error(request,'Accion prohibida: No está autenticado')
         return redirect('home')
-    print("USER ID: %s" % id )  
+      
     name= User.objects.get(username=request.user.username) 
-    result_set = Favorite.objects.filter(name_id=name)
-    context ={'coffees': result_set, 'detail_id': 9999 }
+    result_set = Favorite.objects.filter(name_id=name, is_active=True)
+    context ={'coffees': result_set, 'detail_id': 9999, 'via_detail': False }
     return render(request, 'favorite/favorites_list.html',context)
   
 def add_favorites(request, id):
@@ -25,19 +42,40 @@ def add_favorites(request, id):
     pk = request.user.id
     name= User.objects.get(username=request.user.username)
     house = CoffeeHouse.objects.get(id=id)
-    if not exists_cafeteria(name,house):
-        data_set = Favorite.objects.create(name=name, house=house) 
-        data_set.save()
+    already_exists = False
+    if exists_cafeteria(name,house):
         data_set = Favorite.objects.filter(name=name)
-        messages.success(request,'Felicidades, tienes una cafetería más en favoritas')
-        context = {'coffees': data_set, 'detail_id': id}
+        context = {'coffees':data_set, 'detail_id': id, 'via_detail': True}
+        messages.error(request,'Acción incorrecta: Anteriormente ya había agregado a favoritos')     
         return render(request,'favorite/favorites_list.html', context)
-    
-    messages.error(request,'Acción incorrecta: Anteriormente ya había agregado a favoritos') 
+
+    data_set = Favorite.objects.create(name=name, house=house, is_active=True) 
+    data_set.save()
     data_set = Favorite.objects.filter(name=name)
-    context = {'coffees': data_set, 'detail_id': id}
+    messages.success(request,'Felicidades, tienes una cafetería más en favoritas')
+    context = {'coffees': data_set, 'detail_id': id, 'via_detail': True}
     return render(request,'favorite/favorites_list.html', context)
 
+    '''
+    if not exists_cafeteria(name,house):
+        data_set = Favorite.objects.create(name=name, house=house, is_active=True) 
+        data_set.save()
+        already_exists = True
+        data_set = Favorite.objects.filter(name=name)
+        messages.success(request,'Felicidades, tienes una cafetería más en favoritas')
+        context = {'coffees': data_set, 'detail_id': id, 'via_detail': True}
+        return render(request,'favorite/favorites_list.html', context)
+    else: 
+        
+      
+    data_set = Favorite.objects.filter(name=name)
+    context = {'coffees':data_set, 'detail_id': id, 'via_detail': True}
+    return render(request,'favorite/favorites_list.html', context)
+
+    '''
+    
+def favorite_fails(request):
+    pass
 def exists_cafeteria(user_name, house_name):
     return True if Favorite.objects.filter(name=user_name, house=house_name) else False
         
